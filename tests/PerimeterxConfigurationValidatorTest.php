a3276135e794885143fefea2b049c7cd9b7e8512
bendpx <ben@perimeterx.com>
2017-03-13 11:02:33 +0200

    added px appid to test

--- tests/PerimeterxConfigurationValidatorTest.php
+++ tests/PerimeterxConfigurationValidatorTest.php
@@ -1,60 +1,60 @@
 <?php
 
-  use PHPUnit\Framework\TestCase;
-  use Perimeterx\Perimeterx;
+use PHPUnit\Framework\TestCase;
+use Perimeterx\Perimeterx;
 
-  class PerimeterxConfigurationValidatorTest extends PHPUnit_Framework_TestCase
-  {
+class PerimeterxConfigurationValidatorTest extends PHPUnit_Framework_TestCase
+{
 
     protected $params;
     protected $px;
 
     protected function setUp()
     {
-      $this->params = [
-        'app_id' => PX_APP_ID,
-        'cookie_key' => PX_COOKIE_KEY,
-        'auth_token' => PX_AUTH_TOKEN,
-        'blocking_score' => 80,
-        'captcha_enabled' => false
-      ];
+        $this->params = [
+            'app_id' => PX_APP_ID,
+            'cookie_key' => PX_COOKIE_KEY,
+            'auth_token' => PX_AUTH_TOKEN,
+            'blocking_score' => 80,
+            'captcha_enabled' => false
+        ];
     }
 
-    protected function tearDown(){
-      $reflection = new ReflectionClass($this->px);
-      $instance = $reflection->getProperty('instance');
-      $instance->setAccessible(true);
-      $instance->setValue(null, null);
-      $instance->setAccessible(false);
+    protected function tearDown()
+    {
+        $reflection = new ReflectionClass($this->px);
+        $instance = $reflection->getProperty('instance');
+        $instance->setAccessible(true);
+        $instance->setValue(null, null);
+        $instance->setAccessible(false);
     }
 
 
     public function testPxConfigurationAPIPath()
     {
-      $this->px = Perimeterx::Instance($this->params);
-      $pxConfig = $this->px->getPxConfig();
+        $this->px = Perimeterx::Instance($this->params);
+        $pxConfig = $this->px->getPxConfig();
 
-      $this->assertEquals($pxConfig['perimeterx_server_host'],'https://sapi-pxmi1fumjs.perimeterx.net');
+        $this->assertEquals($pxConfig['perimeterx_server_host'], 'https://sapi-' + PX_APP_ID + '.perimeterx.net');
     }
 
     public function testPxConfigurationCustomization()
     {
-      $customParams = array_merge([
-        'css_ref' => 'http://www.google.com/stylesheet.css',
-        'js_ref' => 'http://www.google.com/script.js',
-        'custom_logo' => 'http://www.google.com/logo.png',
-      ],$this->params);
+        $customParams = array_merge([
+            'css_ref' => 'http://www.google.com/stylesheet.css',
+            'js_ref' => 'http://www.google.com/script.js',
+            'custom_logo' => 'http://www.google.com/logo.png',
+        ], $this->params);
 
-      $this->px = Perimeterx::Instance($customParams);
-      $pxConfig = $this->px->getPxConfig();
+        $this->px = Perimeterx::Instance($customParams);
+        $pxConfig = $this->px->getPxConfig();
 
-      $this->assertEquals($pxConfig['custom_logo'],'http://www.google.com/logo.png');
-      $this->assertEquals($pxConfig['js_ref'],'http://www.google.com/script.js');
-      $this->assertEquals($pxConfig['css_ref'],'http://www.google.com/stylesheet.css');
+        $this->assertEquals($pxConfig['custom_logo'], 'http://www.google.com/logo.png');
+        $this->assertEquals($pxConfig['js_ref'], 'http://www.google.com/script.js');
+        $this->assertEquals($pxConfig['css_ref'], 'http://www.google.com/stylesheet.css');
     }
 
-  }
-
+}
 
 
 ?>
