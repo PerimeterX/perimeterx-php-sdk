44e73639d963af0d3e0b32f708c285c01b2e1bde
pxjohnny <johnny@perimeterx.com>
2018-12-23 15:46:23 +0200

    update file format

--- src/PerimeterxUtils.php
+++ src/PerimeterxUtils.php
@@ -5,26 +5,26 @@ namespace Perimeterx;
 class PerimeterxUtils
 {
 	private $customParamsArray = [
-        'custom_param1' => '',
-        'custom_param2' => '',
-        'custom_param3' => '',
-        'custom_param4' => '',
-        'custom_param5' => '',
-        'custom_param6' => '',
-        'custom_param7' => '',
-        'custom_param8' => '',
-        'custom_param9' => '',
-        'custom_param10' => ''
-    ];
+		'custom_param1' => '',
+		'custom_param2' => '',
+		'custom_param3' => '',
+		'custom_param4' => '',
+		'custom_param5' => '',
+		'custom_param6' => '',
+		'custom_param7' => '',
+		'custom_param8' => '',
+		'custom_param9' => '',
+		'custom_param10' => ''
+	];
 
 	public function handleCustomParams($pxConfig, &$array) {
 		if (isset($pxConfig['enrich_custom_params'])) {
-            $riskCustomParams = $pxConfig['enrich_custom_params']($customParamsArray);
-            foreach ($riskCustomParams as $key => $value) {
-                if (preg_match('/custom_param\d+$/i', $key) && $value != '') {
-                    $array[$key] = $value;
-                }
-            }
-        }
+			$riskCustomParams = $pxConfig['enrich_custom_params']($customParamsArray);
+			foreach ($riskCustomParams as $key => $value) {
+				if (preg_match('/custom_param\d+$/i', $key) && $value != '') {
+					$array[$key] = $value;
+				}
+			}
+		}
 	}
 }
