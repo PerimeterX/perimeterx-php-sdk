adef47a9b0aa9b2e1da93ddf0e203b2c949ef7a7
Ori Gold <ori.gold@perimeterx.com>
2021-05-06 22:03:15 +0300

    PerimeterxFieldExtractor & Manager: added

---
+++ src/PerimeterxFieldExtractor.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Perimeterx;
+
+
+class PerimeterxFieldExtractor {
+
+    private static $NESTED_OBJECT_SEPARATOR = ".";
+    private static $HASH_ALGO = "sha256";
+    private $sentThrough;
+    private $contentType;
+    private $encoding;
+    private $extractionFields;
+
+    /**
+     * @param string, @param string, @param string, @param array
+     */
+    public function __construct($sentThrough, $contentType, $encoding, $desiredFields) {
+        $this->sentThrough = $sentThrough;
+        $this->contentType = $contentType;
+        $this->encoding = $encoding;
+        $this->extractionFields = $desiredFields;
+    }
+
+    /**
+     * @return array
+     */
+    public function extractFields() {
+        $container = $this->getContainer();
+        if (empty($container)) {
+            return null;
+        }
+        $extractedCredentials = $this->getFieldsFrom($container, $this->extractionFields);
+        if (empty($extractedCredentials)) {
+            return null;
+        }
+
+        return array_map(function($value) {
+            return hash(PerimeterxFieldExtractor::$HASH_ALGO, $value);
+        }, $extractedCredentials);
+    }
+
+    private function getContainer() {
+        switch ($this->sentThrough) {
+            case "body":
+                return $this->getBodyContainer();
+            case "header":
+                return $this->getHeaderContainer();
+            case "query-param":
+                return $this->getQueryContainer();
+            default:
+                return null;
+        }
+    }
+
+    private function getBodyContainer() {
+        switch ($this->contentType) {
+            case "json":
+                return $this->getJsonBodyContainer();
+            case "form":
+            case "form-data":
+                return $this->getFormBodyContainer();
+            default:
+                return null;
+        }
+    }
+
+    private function getFormBodyContainer() {
+        switch ($this->encoding) {
+            case "url-encode":
+            case "url-encoded":
+                return $this->getUrlEncodedBodyContainer();
+            case "clear-text": 
+                return $this->getMultipartFormDataBodyContainer();
+            default:
+                return null;
+        }
+    }
+
+    private function getHeaderContainer() {
+        $headers = getallheaders();
+        foreach ($headers as $name => $value) {
+            $headers[$name] = utf8_encode($value);
+        }
+        return $headers;
+    }
+
+    private function getQueryContainer() {
+        $queryString = $_SERVER['QUERY_STRING'];
+        parse_str($queryString, $queryParams);
+        return $queryParams;
+    }
+
+    private function getUrlEncodedBodyContainer() {
+        $body = PerimeterxUtils::getPostRequestBody();
+        parse_str($body, $form);
+        return $form;
+    }
+
+    private function getMultipartFormDataBodyContainer() {
+        return $_REQUEST;
+    }
+
+    private function getJsonBodyContainer() {
+        $jsonBody = PerimeterxUtils::getPostRequestBody();
+        if (empty($jsonBody)) {
+            return null;
+        }
+        $jsonArray = json_decode($jsonBody, true);
+        if (empty($jsonArray)) {
+            return null;
+        }
+        return $jsonArray;
+    }
+
+    private function getFieldsFrom(&$container, &$desiredFields) {
+        if (empty($container)) {
+            return null;
+        }
+
+        $fields = [];
+        foreach ($desiredFields as $originalRequestFieldName => $resultActivityFieldName) {
+            $propertyArray = explode(PerimeterxFieldExtractor::$NESTED_OBJECT_SEPARATOR, $originalRequestFieldName);
+            $value = PerimeterxUtils::getNestedArrayProperty($container, $propertyArray);
+            if (!empty($value)) {
+                $fields[$resultActivityFieldName] = $value;
+            }
+        }
+        return $fields;
+    }
+}
---
+++ src/PerimeterxFieldExtractorManager.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Perimeterx;
+
+use Psr\Log\LoggerInterface;
+
+class PerimeterxFieldExtractorManager {
+
+    private static $USERNAME_FIELD = "user";
+    private static $PASSWORD_FIELD = "pass";
+
+    private $extractorMap;
+    private $logger;
+
+    public function __construct(array $extractorMap, $logger) {
+        $this->extractorMap = $extractorMap;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return array | null
+     */
+    public function extractFields() {
+        try {
+            $uriWithoutQuery = strtok($_SERVER['REQUEST_URI'], "?");
+            $extractorKey = self::generateMapKey($uriWithoutQuery, $_SERVER["REQUEST_METHOD"]);
+            $extractor = $this->extractorMap[$extractorKey];
+            return isset($extractor) ? $extractor->extractFields() : null;
+        } catch (\Exception $e) {
+            $this->logger->error("Exception thrown while extracting fields: " . $e->getMessage());
+        } catch (\Error $e) {
+            $this->logger->error("Error thrown while extracting fields: " . $e->getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * @param array of arrays
+     * @return array
+     */
+
+    public static function createExtractorMap(array &$extractConfigs) {
+        $extractorMap = array();
+        foreach ($extractConfigs as $extractConfig) {
+            if (!isset($extractConfig["path"], $extractConfig["method"], $extractConfig["sentThrough"], $extractConfig["contentType"], 
+                       $extractConfig["encoding"], $extractConfig["passField"], $extractConfig["userField"])) {
+                continue;
+            }
+            $extractorKey = self::generateMapKey($extractConfig["path"], $extractConfig["method"]);
+            $extractorMap[$extractorKey] = new PerimeterxFieldExtractor(
+                $extractConfig["sentThrough"], $extractConfig["contentType"], $extractConfig["encoding"],
+                array(
+                    $extractConfig["userField"] => PerimeterxFieldExtractorManager::$USERNAME_FIELD,
+                    $extractConfig["passField"] => PerimeterxFieldExtractorManager::$PASSWORD_FIELD
+                ));
+        }
+        return $extractorMap;
+    }
+
+    /**
+     * @param string, @param string
+     * @return string
+     */
+    public static function generateMapKey($requestPath, $requestMethod) {
+        return $requestPath . ":" . strtoupper($requestMethod);
+    }
+}
